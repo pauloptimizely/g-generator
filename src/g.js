796104c8b94a15941ac5b2d01b5415f030b6d611
pauloptimizely <paul.serraino@optimizely.com>
2018-08-05 16:14:49 -0500

    make g.js executable

--- src/g.js
+++ src/g.js
@@ -1,6 +1,6 @@
+#!/usr/bin/env node
+
 const argv = require('minimist')(process.argv.slice(2));
-const fs = require('fs')
-const path = require('path')
 
 if (argv._.length < 1) {
   console.log('g [generator] [options]');
